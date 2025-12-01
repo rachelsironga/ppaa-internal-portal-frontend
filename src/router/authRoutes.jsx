cbf83c03ca0401cca358fb69c047bad38a97acd3
patrickNachenga <patricknachenga@gmail.com>
2025-12-01 17:05:56 +0300

    save chnage

---
+++ src/router/authRoutes.jsx
@@ -0,0 +1,21 @@
+import { Route } from "react-router-dom";
+import LoginPage from "../pages/authentication/LoginPage";
+import NewUserPage from "../pages/authentication/NewUserPage";
+import { ForgotPasswordPage } from "../pages/authentication/ForgotPasswordPage";
+import RegisterPage from "../pages/authentication/RegisterPage";
+
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <RegisterPage />;
+}
+export const authRoutes = (
+  <>
+    <Route path="/auth/login" element={<LoginPage />} />
+    <Route path="/auth/register" element={<RegisterAndLogout />} />
+    <Route path="/auth/forgot-password" element={<ForgotPasswordPage />} />
+    <Route
+      path="/auth/new-user-0InEm7BVGIrZafX2riM8DQFgQG2L06ImZlP3oJF"
+      element={<NewUserPage />}
+    />
+  </>
+);
